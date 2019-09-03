a7f5386704e9181102e6ec738bddf30b3931524b
Vasily Galkin <galkin-vv@ya.ru>
2019-09-03 16:37:43 +0000

    unified and simplified extension building

--- lib/make.sh
+++ lib/make.sh
@@ -5,31 +5,20 @@ cd "$SELF_DIR"
 
 python3 setup_bbox.py build_ext --inplace
 rm -rf build
+mkdir -p build
+cp -rv model/csrc build/csrc
 HIP_DIR=/opt/rocm/hip
 if [ -d "$HIP_DIR" ]; then
 	export HIP_DIR
-	mkdir -p build
-	cp -rv model/csrc build/hipsrc
-	$HIP_DIR/bin/hipify-perl --inplace --print-stats build/hipsrc/cuda/*.cu
+	$HIP_DIR/bin/hipify-perl --inplace --print-stats build/csrc/cuda/*.cu
 
-	#torch-specific replacements
-	sed -i 's!ATen/cuda/CUDAContext.h!ATen/hip/HIPContext.h!g' build/hipsrc/cuda/*.cu
-	sed -i 's!THC/THC!THH/THH!g' build/hipsrc/cuda/*.cu
-	sed -i 's!getCurrentCUDAStream!getCurrentHIPStream!g' build/hipsrc/cuda/*.cu
-	export CUDA_PATH=$SELF_DIR #contains bin/nvcc redirector
-
-	python3 setup_hip_layers.py build
+	#torch-specific replacements for hip
+	sed -i 's!ATen/cuda/CUDAContext.h!ATen/hip/HIPContext.h!g' build/csrc/cuda/*.cu
+	sed -i 's!THC/THC!THH/THH!g' build/csrc/cuda/*.cu
+	sed -i 's!getCurrentCUDAStream!getCurrentHIPStream!g' build/csrc/cuda/*.cu
+	#export CUDA_PATH=$SELF_DIR #contains bin/nvcc redirector
 else
-	CUDA_PATH=/usr/local/cuda/
-
-	# Choose cuda arch as you need
-	CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
-           -gencode arch=compute_35,code=sm_35 \
-           -gencode arch=compute_50,code=sm_50 \
-           -gencode arch=compute_52,code=sm_52 \
-           -gencode arch=compute_60,code=sm_60 \
-           -gencode arch=compute_61,code=sm_61 "
-	#  -gencode arch=compute_70,code=sm_70 "
-	python3 setup_layers.py build
+	export CUDA_PATH=/usr/local/cuda/
 fi
+python3 setup_layers.py build --debug
 mv -v build/lib.*/model/_C.*.so model
